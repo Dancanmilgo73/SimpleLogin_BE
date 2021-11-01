7bc0b13580109bfb5cb2f5939864e67fa8ce1139
Dan Ngetich <dancanmilgo73@gmail.com>
2021-11-01 12:39:42 +0300

    erroe handling

--- server.js
+++ server.js
@@ -12,31 +12,41 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signup", async (req, res) => {
-  const regex = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
-  );
   const { email, username, password, confirmPassword } = req.body;
-  if (!email || !username || !password || !confirmPassword) {
-    res.status(406).send("fill in all credentials");
-  }
-  if (!regex.test(password)) {
-    res
-      .status(406)
-      .send(
-        "Password must be atleast 8 characters long and contain small letters, capital letters, special characters and numbers"
-      );
-  }
   try {
+    if (!email || !username || !password || !confirmPassword)
+      res.status(401).send("fill in all credentials");
+
+    if (password !== confirmPassword)
+      res.status(401).send("Confirm password do not match");
+
+    checkPasswordStrength(password);
     // const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = { email, name: username, password: hashedPassword };
     users.push(user);
     console.log(users);
+    res.status(201).send("User was added");
   } catch {
     res.status(500).send();
   }
-  res.status(201).send("User was added");
-  // return;
+  function checkPasswordStrength(password) {
+    const checkForLength = new RegExp("^(?=.{8,})");
+    const checkForSymbols = new RegExp("^(?=.*[!@#$%^&*])");
+    const checkForCapsLettersNumbers = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])"
+    );
+    if (!checkForLength.test(password))
+      res.status(401).send("Password must be atleast 8 characters long ");
+
+    if (!checkForSymbols.test(password))
+      res.status(401).send("Password must contain atleast 1 symbol");
+
+    if (!checkForCapsLettersNumbers.test(password))
+      res
+        .status(401)
+        .send("Password must contain small, letters caps and numbers");
+  }
 });
 app.listen(port, () => {
   console.log(`app listenig on port ${port}`);
