0532f8eade1726ee0405f135d1ce4e931370527b
Dancanmilgo73 <dancanmilgo73@gmail.com>
2021-11-05 09:40:32 +0300

    connected  to mysql

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
+const mysql = require('mysql');
 const port = 3000;
+const connection= require("./db");
 
 const users = [];
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.post("/signup", async (req, res) => {
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
+
+
+app.post("/register", async (req, res) => {
   const { email, username, password, confirmPassword } = req.body;
   try {
     if (!email || !username || !password || !confirmPassword)
@@ -23,8 +34,10 @@ app.post("/signup", async (req, res) => {
     // const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = { email, name: username, password: hashedPassword };
-    users.push(user);
-    console.log(users);
+    let sql = `INSERT INTO users(name, email, password) values("${user.name}", "${user.email}", "${user.password}")`;
+    connection.query(sql);
+    // users.push(user);
+    // console.log(users);
     res.status(201).send("User was added");
   } catch {
     res.status(500).send();
@@ -54,9 +67,17 @@ app.post("/login", async (req, res) => {
 
   try {
     if (email && password) {
-      const user = users.find((user) => user.email === email);
-
-      if (!user) res.status(401).send("User not found");
+      // const user = users.find((user) => user.email === email);
+  let sql = `SELECT * FROM users WHERE email = "${email}"`;
+  connection.query(sql, (err,result)=>{
+  if(err){
+    console.log(err);
+    return res.sendStatus(401);
+    
+  }
+  const user = result.find(user => user.email==email);
+//  console.log(user.name);
+  if (!user) res.status(401).send("User not found");
       bcrypt.compare(password, user.password, (err, result) => {
         if (!result) res.status(401).send("Wrong credentials");
 
@@ -67,11 +88,29 @@ app.post("/login", async (req, res) => {
         );
         res.json({ accessToken: token });
       });
+  
+});
+// console.log(user); 
+   
+
     }
   } catch {
     res.sendStatus(500);
   }
 });
+app.get("/users" ,(req,res)=>{
+res.send("hello users")
+});
+function Authenticator(req, res, next){
+  const authHeader= req.headers['authorization'];
+  const token = authHeader.split(' ')[1];
+  if(token == null) return res.sendStatus(401);
+  JWT.verify(token, process.env.ACCESS_TOKEN, (err, user)=>{
+    if(err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  })
+}
 
 app.listen(port, () => {
   console.log(`app listenig on port ${port}`);
